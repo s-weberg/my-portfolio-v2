046100581def25b5ebb6c25226c61271437822b0
s-weberg <sandraweberg90@gmail.com>
2025-05-07 21:02:56 +0200

    updated app jsx

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import React from 'react';
+import React, { useState } from 'react';
 import Bank from './components/bank';
 import Books from './components/Books';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -8,9 +8,30 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Navbar from './Navbar';
+import PopupWindow from './components/Popup';
 
 function App() {
+
+  const [popupInfo, setPopupInfo] = useState({
+    title: "This is a window with information",
+    info: "This is the information",
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+  
   return <>
+  <div>
+    <button onClick={setIsOpen(true)}>Open popup</button>
+    {isOpen && (
+      <PopupWindow
+      title={popupInfo.title}
+      info={popupInfo.info}
+      onClick={closePopup}
+      />
+    )}
+  </div>
   <BrowserRouter>
   <Navbar></Navbar>
   <Routes>
@@ -21,5 +42,6 @@ function App() {
   </BrowserRouter>
   </>
 }
+
 
 export default App;
